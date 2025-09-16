a2a8fd4a4c4245701fb4ad8401f45860660ac1fa
suhrud55 <suhrudvalaparla55@gmail.com>
2025-09-16 12:10:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
+    ami = "ami-0b982602dbb32c5bd"
     instance_type = "t2.micro"
     tags = {
       Name = "prod"
